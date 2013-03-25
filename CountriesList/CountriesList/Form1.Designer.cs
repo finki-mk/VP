fc85ecf57721cb5bfeeb780dda70420332384a7b
Tomche Delev <tomce.delev@gmail.com>
2013-03-25 17:17:01 +0100

    Added initial visual appearance of CountriesList
    Only the form

---
+++ CountriesList/CountriesList/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace CountriesList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbC1 = new System.Windows.Forms.RadioButton();
+            this.btnNextQuestion = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbShort = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbCaptial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbC3 = new System.Windows.Forms.RadioButton();
+            this.rbC2 = new System.Windows.Forms.RadioButton();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbCountries = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbC1
+            // 
+            this.rbC1.AutoSize = true;
+            this.rbC1.Location = new System.Drawing.Point(13, 67);
+            this.rbC1.Name = "rbC1";
+            this.rbC1.Size = new System.Drawing.Size(14, 13);
+            this.rbC1.TabIndex = 3;
+            this.rbC1.TabStop = true;
+            this.rbC1.UseVisualStyleBackColor = true;
+            // 
+            // btnNextQuestion
+            // 
+            this.btnNextQuestion.Location = new System.Drawing.Point(13, 20);
+            this.btnNextQuestion.Name = "btnNextQuestion";
+            this.btnNextQuestion.Size = new System.Drawing.Size(181, 23);
+            this.btnNextQuestion.TabIndex = 2;
+            this.btnNextQuestion.Text = "?";
+            this.btnNextQuestion.UseVisualStyleBackColor = true;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Enabled = false;
+            this.btnGuess.Location = new System.Drawing.Point(6, 130);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(187, 23);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Погоди";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(119, 142);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 9;
+            this.btnRemove.Text = "Избриши";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(10, 142);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Додади";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // tbShort
+            // 
+            this.tbShort.Location = new System.Drawing.Point(10, 116);
+            this.tbShort.Name = "tbShort";
+            this.tbShort.Size = new System.Drawing.Size(100, 20);
+            this.tbShort.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Скратено име:";
+            // 
+            // tbCaptial
+            // 
+            this.tbCaptial.Location = new System.Drawing.Point(10, 77);
+            this.tbCaptial.Name = "tbCaptial";
+            this.tbCaptial.Size = new System.Drawing.Size(184, 20);
+            this.tbCaptial.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Главен град:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Вкупно погодени:";
+            // 
+            // rbC3
+            // 
+            this.rbC3.AutoSize = true;
+            this.rbC3.Location = new System.Drawing.Point(13, 107);
+            this.rbC3.Name = "rbC3";
+            this.rbC3.Size = new System.Drawing.Size(14, 13);
+            this.rbC3.TabIndex = 5;
+            this.rbC3.TabStop = true;
+            this.rbC3.UseVisualStyleBackColor = true;
+            // 
+            // rbC2
+            // 
+            this.rbC2.AutoSize = true;
+            this.rbC2.Location = new System.Drawing.Point(13, 87);
+            this.rbC2.Name = "rbC2";
+            this.rbC2.Size = new System.Drawing.Size(14, 13);
+            this.rbC2.TabIndex = 4;
+            this.rbC2.TabStop = true;
+            this.rbC2.UseVisualStyleBackColor = true;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(10, 37);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(184, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblPoints);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.rbC3);
+            this.groupBox2.Controls.Add(this.rbC2);
+            this.groupBox2.Controls.Add(this.rbC1);
+            this.groupBox2.Controls.Add(this.btnNextQuestion);
+            this.groupBox2.Controls.Add(this.lblQuestion);
+            this.groupBox2.Controls.Add(this.btnGuess);
+            this.groupBox2.Location = new System.Drawing.Point(204, 202);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 188);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Главни градови";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPoints.Location = new System.Drawing.Point(142, 162);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(31, 20);
+            this.lblPoints.TabIndex = 7;
+            this.lblPoints.Text = "0/0";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(10, 50);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(0, 13);
+            this.lblQuestion.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Име:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.tbShort);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbCaptial);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(204, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 173);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Додади држава";
+            // 
+            // lbCountries
+            // 
+            this.lbCountries.FormattingEnabled = true;
+            this.lbCountries.Location = new System.Drawing.Point(12, 26);
+            this.lbCountries.Name = "lbCountries";
+            this.lbCountries.Size = new System.Drawing.Size(186, 368);
+            this.lbCountries.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Листа со држави";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 413);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbCountries);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbC1;
+        private System.Windows.Forms.Button btnNextQuestion;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbShort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbCaptial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbC3;
+        private System.Windows.Forms.RadioButton rbC2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbCountries;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
